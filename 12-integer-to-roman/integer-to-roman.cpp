6e8f45100ac6ccef3ad1fdbb210baa436706b041
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2025-07-03 21:12:20 +0530

    Time: 3 ms (62.69%) | Memory: 9.2 MB (84.37%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        // string romanSymbol[]= {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        // int value[]={1000, 900 , 500, 400, 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4, 1};
+    // string intToRoman(int num) {
+    //     vector<pair<int , string>>storeIntRoman = {
+    //         {1000 ,"M"} , {900 ,"CM"},{500,"D"} ,{400,"CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}
+    //     };
 
-        vector<pair<int , string>>storeIntRoman = {
-            {1000 ,"M"} , {900 ,"CM"},{500,"D"} ,{400,"CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}
-        };
+    //     string ans="";
+    //     for(int i =0; i<13; i++){
+    //         while(num>=storeIntRoman[i].first){
+    //             ans = ans + storeIntRoman[i].second;
+    //             num = num - storeIntRoman[i].first;
+    //         }
+    //     }
+    //     return ans ;
+    // }
+    string intToRoman(int num) {
+        string romanSymbol[]= {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int value[]={1000, 900 , 500, 400, 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4, 1};
 
         string ans="";
         for(int i =0; i<13; i++){
-            while(num>=storeIntRoman[i].first){
-                ans = ans + storeIntRoman[i].second;
-                num = num - storeIntRoman[i].first;
+            while(num>=value[i]){
+                ans = ans + romanSymbol[i];
+                num = num - value[i];
             }
         }
         return ans ;
