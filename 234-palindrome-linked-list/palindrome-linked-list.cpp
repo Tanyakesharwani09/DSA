3b901801599beb1a7ff5f9fc423844d3641c582c
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2025-07-31 19:57:49 +0530

    Time: 0 ms (100.00%) | Memory: 121.9 MB (46.36%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // ListNode* findMiddle(ListNode* head){
+    //     ListNode* slow = head;
+    //     ListNode* fast = head->next;
+
+    //     while(fast != NULL){
+    //         fast=fast->next;
+    //         if(fast != NULL){
+    //             fast = fast->next;
+    //             slow=slow->next;
+    //         }
+    //     }
+    //     return slow;
+    // }
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev= curr;
+            curr=temp;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head->next == NULL){
+            return true;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast != NULL){
+            fast=fast->next;
+            if(fast != NULL){
+                fast = fast->next;
+                slow=slow->next;
+            }
+        }
+        ListNode* reverseKaHead = reverse(slow->next);
+        slow->next = reverseKaHead;
+
+        ListNode*temp1 = head;
+        ListNode*temp2= reverseKaHead;
+
+        while(temp2 != NULL){
+            if(temp1->val != temp2->val){
+                return false;
+            }
+            temp1=temp1->next;
+            temp2 = temp2->next;
+        }
+        return true;
+    }
+};
