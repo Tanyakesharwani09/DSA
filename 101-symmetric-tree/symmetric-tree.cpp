7478c69c1b71e5af464f14bc02b5ebab1256e29b
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2025-09-25 11:19:19 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (28.59%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) return true;
+        bool ans = isMirror(root->left, root->right);
+        return ans;
+    }
+
+    bool isMirror(TreeNode* p, TreeNode* q) {
+        // If both nodes are NULL, they are identical
+        if (p == NULL && q == NULL) {
+            return true;
+        }
+        // If only one of the nodes is NULL, they are not identical
+        if (p == NULL || q == NULL) {
+            return false;
+        }
+        // Check if values are equal and recursively check left and right subtrees
+        if (p->val == q->val) {
+            return isMirror(p->left, q->right) && isMirror(p->right, q->left);
+        }
+        // Values are not equal, they are not identical
+        return false;
+    }
+};
