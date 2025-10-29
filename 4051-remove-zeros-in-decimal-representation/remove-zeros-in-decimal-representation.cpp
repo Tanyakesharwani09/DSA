15d320d11fcc2eda02818a923b08b51933ddc17a
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2025-10-29 23:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (87.96%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.cpp
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    long long removeZeros(long long n) {
-        string input = to_string(n);
-        string ans ="";
-        for(int i=0; i< input.size() ;  i++){
-            if(input[i] != '0'){
-                ans = ans + input[i];
+    // long long removeZeros(long long n) {
+    //     string input = to_string(n);
+    //     string ans ="";
+    //     for(int i=0; i< input.size() ;  i++){
+    //         if(input[i] != '0'){
+    //             ans = ans + input[i];
+    //         }
+    //     }
+    //     long long finalAns = stoll(ans);
+    //     return finalAns;
+    // }
+    long long removeZeros(long long n){
+        long long ans =0 , multi = 1;
+        while(n > 0){
+            int a = n%10;
+            if(a != 0){
+                ans += a * multi;
+                multi *= 10;
             }
+            n = n/10;
         }
-        long long finalAns = stoll(ans);
-        return finalAns;
+        return ans;
+        
     }
+
 };
