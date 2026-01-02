d6101b81363ce04dee8003052e0c55abdbfd207f
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2026-01-02 19:42:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.07%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    int solveUsingRecursion(vector<int>&nums, int index, vector<int>&dp){
-        //bc
-        if(index >= nums.size()){
-            return 0;
-        }
-        //ans already exist
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        int include = nums[index] + solveUsingRecursion(nums, index + 2, dp);
-        int exclude = 0 + solveUsingRecursion(nums, index + 1,dp);
-        dp[index] = max(include , exclude);
-        return dp[index];
+    // int solveUsingRecursion(vector<int>&nums, int index, vector<int>&dp){
+    //     //bc
+    //     if(index >= nums.size()){
+    //         return 0;
+    //     }
+    //     //ans already exist
+    //     if(dp[index] != -1){
+    //         return dp[index];
+    //     }
+    //     int include = nums[index] + solveUsingRecursion(nums, index + 2, dp);
+    //     int exclude = 0 + solveUsingRecursion(nums, index + 1,dp);
+    //     dp[index] = max(include , exclude);
+    //     return dp[index];
+    // }
+
+    int solveUsingTabulation(vector<int>&nums){
+        //step1
+        int n = nums.size();
+        vector<int>dp(n, -1);
+       
+       //step2
+       dp[n-1] = nums[n-1];
+       for(int index = n-2; index>=0; index--){
+            int tempAns =0;
+            if(index +2 <n){
+                tempAns = dp[index +2];
+            }
+            int include = nums[index] + tempAns;
+            int exclude = 0 + dp[index+1];
+            dp[index] = max(include , exclude);
+       }
+       return dp[0];
     }
+       
 
     int rob(vector<int>& nums){
-        vector<int>dp(nums.size(), -1);
-        int index= 0;
-        int ans = solveUsingRecursion(nums,index,dp);
+        int ans = solveUsingTabulation(nums);
         return ans;
         
     }
