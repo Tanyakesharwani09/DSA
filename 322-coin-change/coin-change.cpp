652488723cee85c96ce31eff16b748d5ee30ccee
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2026-01-09 13:43:25 +0530

    Time: 20 ms (70.74%) | Memory: 17.8 MB (80.10%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -18,24 +18,46 @@ public:
     //     return mini;
     // }
 
-    int solveUsingMem(vector<int>&coins, int amount, vector<int>&dp){
-        if(amount == 0){
-            return 0;
-        }
-        if(dp[amount] != -1){
-            return dp[amount];
-        }
-        int mini = INT_MAX;
-        for(int i=0; i<coins.size(); i++){
-            if(amount - coins[i]>=0){
-                int recursionKaAns = solveUsingMem(coins, amount - coins[i],dp);
-                if(recursionKaAns != INT_MAX){
-                    int ans = 1 + recursionKaAns;
-                    mini = min(mini, ans);
+    // int solveUsingMem(vector<int>&coins, int amount, vector<int>&dp){
+    //     if(amount == 0){
+    //         return 0;
+    //     }
+    //     if(dp[amount] != -1){
+    //         return dp[amount];
+    //     }
+    //     int mini = INT_MAX;
+    //     for(int i=0; i<coins.size(); i++){
+    //         if(amount - coins[i]>=0){
+    //             int recursionKaAns = solveUsingMem(coins, amount - coins[i],dp);
+    //             if(recursionKaAns != INT_MAX){
+    //                 int ans = 1 + recursionKaAns;
+    //                 mini = min(mini, ans);
+    //             }
+    //         }
+    //     }
+    //     dp[amount] = mini;
+    //     return dp[amount];
+    // }
+
+
+    int solveUsingTabulation(vector<int>&coins, int amount){
+        vector<int>dp(amount+1, INT_MAX);
+
+        dp[0] = 0;
+
+        for(int value = 1; value <= amount;  value++){
+            int mini = INT_MAX;
+            for(int i =0; i<coins.size(); i++){
+                if(value - coins[i] >=0){
+                    int recursionKaAns = dp[value - coins[i]];
+                    if(recursionKaAns != INT_MAX){
+                        int ans = 1 + recursionKaAns;
+                        mini = min(mini ,ans);
+                    }
                 }
             }
+            dp[value] = mini;
         }
-        dp[amount] = mini;
         return dp[amount];
     }
 
@@ -45,8 +67,15 @@ public:
     //         return -1;
     //    }
     //     return ans;
-        vector<int>dp(amount+1, -1);
-        int ans = solveUsingMem(coins, amount , dp);
+
+        // vector<int>dp(amount+1, -1);
+        // int ans = solveUsingMem(coins, amount , dp);
+        // if(ans == INT_MAX){
+        //     return -1;
+        // }
+        // return ans;
+
+        int ans = solveUsingTabulation(coins, amount);
         if(ans == INT_MAX){
             return -1;
         }
