d4656bb184dcd9bf67ed71ed366aac7acc5abf80
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2026-02-15 15:36:55 +0530

    Time: 2 ms (93.91%) | Memory: 10.4 MB (96.78%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -76,12 +76,42 @@ public:
         return dp[0][0];
     }
 
+    int solveUsingTabulationSO(string&a, string&b){
+        vector<int>curr(a.length()+1 , 0);
+        vector<int>next(a.length()+1 , 0);
+
+        for(int row =0; row <= a.length(); row++ ){
+            next[row] = a.length() - row;
+        }
+
+        for(int j_index = b.length()-1; j_index >=0; j_index--){
+            curr[a.length()] = b.length() - j_index;
+            for(int i_index = a.length()-1; i_index >=0; i_index--){
+                int ans =0;
+                if(a[i_index] == b[j_index]){
+                    ans =0+ next[i_index +1];
+                }
+                else{
+                    int insert = 1 + next[i_index];
+                    int del = 1 + curr[i_index+1];
+                    int replace = 1 + next[i_index+1];
+                    ans = min(insert , min(del , replace));
+                }
+                curr[i_index] = ans;
+            }
+            //shift
+            next= curr;
+        }
+        return next[0];
+    }
+
     int minDistance(string word1, string word2) {
         // int ans = solveUsingRecursion(word1, word2 , 0 , 0);
         // vector<vector<int>>dp(word1.length()+1 ,vector<int>(word2.length()+1 , -1));
         // int ans = solveUsingMem(word1, word2 , 0 , 0, dp);
 
-        int ans = solveUsingTabulation(word1 , word2);
+        // int ans = solveUsingTabulation(word1 , word2);
+        int ans = solveUsingTabulationSO(word1 , word2);
 
         return ans;
     }
