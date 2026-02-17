53d4d5258ef9e85e969efa9569d0c2c053303eb8
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2026-02-17 15:01:37 +0530

    Time: 34 ms (86.26%) | Memory: 11 MB (55.43%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -18,19 +18,41 @@ public:
         if(dp[start][end] != -1){
             return dp[start][end];
         }
-         int maxi =INT_MAX;
+        int maxi =INT_MAX;
         for(int i = start; i<=end;  i++){
             maxi= min(maxi , i + max(solveUsingMem(start , i-1, dp), solveUsingMem(i+1 , end, dp)));
         }
         dp[start][end] = maxi;
         return dp[start][end];
-
     }
+
+    int solveUsingTabulation(int n){
+        vector<vector<int>>dp(n+2 , vector<int>(n+2, 0));
+
+        for(int start = n; start >=1; start--){
+            for(int end = start; end <=n; end++){
+                if(start == end){
+                    continue;
+                }
+                else{
+                    int maxi =INT_MAX;
+                    for(int i = start; i<=end;  i++){
+                        maxi= min(maxi , i + max(dp[start][i-1], dp[i+1][end]));
+                    }
+                    dp[start][end] = maxi;
+                }
+            }
+        }
+        return dp[1][n];
+    }
+
     int getMoneyAmount(int n) {
         
         // int ans = solveUsingRecursion(1, n);
-        vector<vector<int>>dp(n+1 , vector<int>(n+1, -1));
-        int ans = solveUsingMem(1, n, dp);
+        // vector<vector<int>>dp(n+1 , vector<int>(n+1, -1));
+        // int ans = solveUsingMem(1, n, dp);
+
+        int ans = solveUsingTabulation(n);
         return ans;
         
     }
