5819a8387c800f91918c8d3ea7b5064c7521bc2e
Tanya Kesharwani <136500817+Tanyakesharwani09@users.noreply.github.com>
2026-05-04 13:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 37.1 MB (82.37%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -11,33 +11,47 @@
  */
 class Solution {
 public:
-    void storeInorder(TreeNode*root , vector<int>&inorder){
-        if(root == NULL){
-            return;
-        }
-        storeInorder(root->left , inorder);
-        inorder.push_back(root->val);
-        storeInorder(root->right , inorder);
+    // bool findTarget(TreeNode* root, int k) {
+        
+    // }
+
+    void inorderTraverse(TreeNode* root, vector<int>&inorder){	
+	if(root == NULL){
+		return;
+    }
+    inorderTraverse(root->left, inorder);
+    inorder.push_back(root->val);
+    inorderTraverse(root->right , inorder);
 
     }
-    bool findTarget(TreeNode* root, int k) {
-        vector<int>inorder;
-        storeInorder(root, inorder);
-        int s =0;
-        int e = inorder.size()-1;
-        while(s < e){
-            int sum = inorder[s] + inorder[e];
-            if(sum==k){
-                return true;
-            }
-            if(sum>k){
-                e--;
+
+    bool findTarget(TreeNode*root, int k ){
+	    if(root == NULL){
+		    return false; 
+        }
+        if(root ->left == NULL && root->right == NULL){
+	        return false;
+        }
+        vector<int>inorder;   // [2 , 3,4 , 5, 6 ,7]    k=10
+        inorderTraverse(root, inorder);
+
+        int left =0;
+        int right = inorder.size() - 1;  //5
+
+        while(left < right){
+
+	        if (( inorder[left] + inorder[right]) == k){
+		        return true;
+            }    
+
+            else if(( inorder[left] + inorder[right]) < k){    //k =10
+                left++;
             }
             else{
-                s++;
+	            right-- ;
             }
-
         }
         return false;
+
     }
 };
